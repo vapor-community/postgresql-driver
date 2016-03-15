8b3f6886b86c8e0d3dacfc5a8e5d00a38e0e11a4
Prince Ugwuh <prince@honestdollar.com>
2016-03-14 22:38:49 -0500

    update fluent to the new driver protocol, added support for prepared statements

--- Sources/PostgreSQLDriver.swift
+++ Sources/PostgreSQLDriver.swift
@@ -9,29 +9,11 @@ public class PostgreSQLDriver: Fluent.Driver {
 
     public init(connectionInfo: String) {
         self.database = PostgreSQL(connectionInfo: connectionInfo)
-            try! self.database.connect()
-        }
-
-    public func fetchOne(table table: String, filters: [Filter]) -> [String: String]? {
-        let sql = SQL(operation: .SELECT, table: table)
-        sql.filters = filters
-        sql.limit = 1
-
-        let statement = self.database.createStatement(withQuery: sql.query)
-        do {
-          if try statement.execute() {
-            if let data = dataFromResult(statement.result) {
-              return data.first
-            }
-          }
-        } catch { /* fail silently (for now) */ }
-        return nil
+        try! self.database.connect()
     }
 
-    public func fetch(table table: String, filters: [Filter]) -> [[String: String]] {
-        let sql = SQL(operation: .SELECT, table: table)
-        sql.filters = filters
-        let statement = self.database.createStatement(withQuery: sql.query)
+    public func execute(dslContext: DSGenerator) -> [[String: StatementValue]]? {
+        let statement = self.database.createStatement(withQuery: dslContext.parameterizedQuery, values: dslContext.queryValues)
         do {
           if try statement.execute() {
             if let data = dataFromResult(statement.result) {
@@ -39,67 +21,18 @@ public class PostgreSQLDriver: Fluent.Driver {
             }
           }
         } catch { /* fail silently (for now) */ }
-        return []
-    }
-
-    public func delete(table table: String, filters: [Filter]) {
-        let sql = SQL(operation: .DELETE, table: table)
-        sql.filters = filters
-
-        let statement = self.database.createStatement(withQuery: sql.query)
-        do {
-          try statement.execute()
-        } catch { /* fail silently (for now) */ }
-    }
-
-    public func update(table table: String, filters: [Filter], data: [String: String]) {
-        let sql = SQL(operation: .UPDATE, table: table)
-        sql.filters = filters
-        sql.data = data
-
-        let statement = self.database.createStatement(withQuery: sql.query)
-        do {
-          try statement.execute()
-        } catch { /* fail silently (for now) */ }
-    }
-
-    public func insert(table table: String, items: [[String: String]]) {
-      for item in items {
-        let sql = SQL(operation: .INSERT, table: table)
-        sql.data = item
-
-        let statement = self.database.createStatement(withQuery: sql.query)
-        do {
-          try statement.execute()
-        } catch { /* fail silently (for now) */ }
-      }
-    }
-
-    public func upsert(table table: String, items: [[String: String]]) {
-        //check if object exists
-    }
-
-    public func exists(table table: String, filters: [Filter]) -> Bool {
-        print("exists \(filters.count) filters on \(table)")
-
-        return false
-    }
-
-    public func count(table table: String, filters: [Filter]) -> Int {
-        print("count \(filters.count) filters on \(table)")
-
-        return 0
+        return nil
     }
 
     // MARK: - Internal
 
-    internal func dataFromResult(result: PSQLResult?) -> [[String: String]]? {
+    internal func dataFromResult(result: PSQLResult?) -> [[String: StatementValue]]? {
       guard let result = result else { return nil }
       if result.rowCount > 0 && result.columnCount > 0 {
-        var data: [[String: String]] = []
+        var data: [[String: StatementValue]] = []
         var row: Int = 0
         while row < result.rowCount {
-            var item: [String: String] = [:]
+            var item: [String: StatementValue] = [:]
             var column: Int = 0
             while column < result.columnCount {
                 item[result.columnName(column) ?? ""] = result.stringAt(row, columnIndex: column)
