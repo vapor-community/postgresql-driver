d180c94b3587d78d3f24ab65c7c485550d28e7bf
Prince Ugwuh <prince.ugwuh@gmail.com>
2016-07-15 14:16:53 -0500

    updated package

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "PostgreSQLDriver",
     dependencies: [
-   		 .Package(url: "https://github.com/qutheory/cpostgresql.git", majorVersion: 0),
+   		 .Package(url: "https://github.com/qutheory/postgresql.git", majorVersion: 0),
 		 .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0)
-        //.Package(url: "../fluent-local", majorVersion: 0)
     ]
 )
