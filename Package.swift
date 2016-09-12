33f173551e1beca5ddc32088c46f7b4f86853aba
Prince Ugwuh <pugwuh@inmotionsoftware.com>
2016-09-12 13:26:07 -0500

    fixed test issue, updated packages

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentPostgreSQL",
     dependencies: [
-        .Package(url: "https://github.com/vapor/postgresql.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 10)
+        .Package(url: "https://github.com/vapor/postgresql.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 11)
     ]
 )
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,7 @@
 #if os(Linux)
 
 import XCTest
-@testable import FluentPostgreSQLTestSuite
+@testable import FluentPostgreSQLTests
 
 XCTMain([
     testCase(PostgreSQLDriverTests.allTests),
