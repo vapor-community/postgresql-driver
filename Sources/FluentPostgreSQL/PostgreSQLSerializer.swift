196a3eea63e83c8db0a2c10d0e846ba00d1498d0
Nate Bird <natebird@gmail.com>
2016-09-28 10:56:54 -0400

    Update serializer to use Node.

--- Sources/FluentPostgreSQL/PostgreSQLSerializer.swift
+++ Sources/FluentPostgreSQL/PostgreSQLSerializer.swift
@@ -3,55 +3,62 @@ import Fluent
 public final class PostgreSQLSerializer: GeneralSQLSerializer {
     var placeholderCount: Int = 0
 
-    public override func sql(_ value: Value) -> String {
+    public override func sql(_ value: Node) -> String {
         self.placeholderCount += 1
         return "$\(self.placeholderCount)"
     }
-    
-    public override func serialize() -> (String, [Value]) {
+
+    public override func serialize() -> (String, [Node]) {
         self.placeholderCount = 0
         return super.serialize()
     }
-    
+
     public override func sql(_ string: String) -> String {
         return "\(string)"
     }
-    
-    public override func sql(_ column: SQL.Column) -> String {
-        switch column {
-        case .primaryKey:
-            return sql("id") + " SERIAL PRIMARY KEY"
-        case .integer(let name):
-            return sql(name) + " INT"
-        case .string(let name, let length):
+
+    public override func sql(_ type: Schema.Field.DataType) -> String {
+        switch type {
+        case .id:
+            return "SERIAL PRIMARY KEY"
+        case .string(let length):
             if let length = length {
-                return sql(name) + " VARCHAR(\(length))"
+                return "VARCHAR(\(length))"
             } else {
-                return sql(name) + " VARCHAR(255)"
+                return "VARCHAR(255)"
             }
-        case .double(let name, _, _):
-            return sql(name) + " FLOAT"
+        case .double:
+            return "FLOAT"
+        default:
+            break
         }
+        return super.sql(type)
     }
-    
-    public override func sql(_ data: [String: Value]) -> (String, [Value]) {
-        var clause: [String] = []
-        
-        var d = data
-        if d.keys.contains("id") {
-            d.removeValue(forKey: "id")
+
+    public override func sql(_ data: Node?) -> (String, [Node])? {
+        guard let node = data else {
+            return nil
         }
-        
-        let values = Array(d.values)
-        
-        clause += sql(keys: Array(d.keys))
+
+        guard case .object(var dict) = node else {
+            return nil
+        }
+
+        var clause: [String] = []
+
+        if dict.keys.contains("id") {
+            dict.removeValue(forKey: "id")
+        }
+
+        let values = Array(dict.values)
+
+        clause += sql(keys: Array(dict.keys))
         clause += "VALUES"
         clause += sql(values)
-        
+
         return (
             sql(clause),
             values
         )
     }
-
 }
