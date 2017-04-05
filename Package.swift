c24d9a39e64fcd4a1bf692a13543c80167e9f23c
Nate Bird <natebird@gmail.com>
2017-04-04 22:31:40 -0400

    Update dependencies to 2.0 betas.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,10 @@ import PackageDescription
 let package = Package(
     name: "FluentPostgreSQL",
     dependencies: [
-        .Package(url: "https://github.com/vapor/postgresql.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1)
+        // PostgreSQL interface for Swift.
+        .Package(url: "https://github.com/vapor/postgresql.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+
+        // Swift models, relationships, and querying for NoSQL and SQL databases.
+        .Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
     ]
 )
