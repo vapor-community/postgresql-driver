df7029c34ef5c2802a55ef726fd411aee1eb814a
Nate Bird <natebird@gmail.com>
2017-04-06 22:37:07 -0400

    Update package name to PostgreSQLDriver

--- Package.swift
+++ Package.swift
@@ -1,12 +1,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "FluentPostgreSQL",
+    name: "PostgreSQLDriver",
     dependencies: [
         // PostgreSQL interface for Swift.
         .Package(url: "https://github.com/vapor/postgresql.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
 
         // Swift models, relationships, and querying for NoSQL and SQL databases.
-        .Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+
+        // Random number generation
+        .Package(url: "https://github.com/vapor/random.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
     ]
 )
