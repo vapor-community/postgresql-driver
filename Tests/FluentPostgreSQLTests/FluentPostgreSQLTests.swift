ae9765f6fe65970c60fc74a2fd60096584707f5b
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-04-26 02:53:25 +0200

    Fix foreign key test and indentation of test sources
    [skip ci] as patching Fluent is required for FKs to work.

--- Tests/FluentPostgreSQLTests/FluentPostgreSQLTests.swift
+++ Tests/FluentPostgreSQLTests/FluentPostgreSQLTests.swift
@@ -14,33 +14,33 @@ class FluentPostgreSQLTests: XCTestCase {
         let tester = Tester(database: database)
 
         do {
-          try tester.testAll()
+            try tester.testAll()
         } catch {
             XCTFail("\(error)")
         }
     }
 
     func testForeignKey() throws {
-       let driver = FluentPostgreSQL.Driver.makeTestConnection()
-       let database = Database(driver)
+        let driver = FluentPostgreSQL.Driver.makeTestConnection()
+        let database = Database(driver)
 
-       defer {
-           try! database.delete(Atom.self)
-           try! database.delete(Compound.self)
-       }
+        defer {
+            try! database.delete(Atom.self)
+            try! database.delete(Compound.self)
+        }
 
-       try database.create(Compound.self) { compounds in
-           compounds.id()
-           compounds.string("foo")
-       }
+        try database.create(Compound.self) { compounds in
+            compounds.id()
+            compounds.string("foo", unique: true)
+        }
 
-       try database.index("foo", for: Compound.self)
+        try database.index("foo", for: Compound.self)
 
-       try database.create(Atom.self) { atoms in
-           atoms.id()
-           atoms.string("name")
-           atoms.foreignKey("name", references: "foo", on: Compound.self)
-       }
+        try database.create(Atom.self) { atoms in
+            atoms.id()
+            atoms.string("name")
+            atoms.foreignKey("name", references: "foo", on: Compound.self)
+        }
 
         try database.index("name", for: Atom.self)
     }
