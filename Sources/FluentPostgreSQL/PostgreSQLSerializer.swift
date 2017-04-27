73b222fadeaebe85567560611eaa97f2028ed4a7
Nate Bird <natebird@gmail.com>
2017-04-27 00:14:51 -0400

    Added back in all the old serializer overrides updated to Fluent 2.0.
    [skip ci] as the tests are still broken.

--- Sources/FluentPostgreSQL/PostgreSQLSerializer.swift
+++ Sources/FluentPostgreSQL/PostgreSQLSerializer.swift
@@ -6,7 +6,15 @@ public final class PostgreSQLSerializer<E: Entity>: GeneralSQLSerializer<E> {
 
     public override func serialize() -> (String, [Node]) {
         positionalIndex = 0
-        return super.serialize()
+
+        let serialized = super.serialize()
+        
+        switch query.action {
+        case .create:
+            return (serialized.0 + " RETURNING \(E.idKey)", serialized.1)
+        default:
+            return serialized
+        }
     }
 
     public override func type(_ type: Field.DataType, primaryKey: Bool) -> String {
@@ -95,10 +103,23 @@ public final class PostgreSQLSerializer<E: Entity>: GeneralSQLSerializer<E> {
         
         statement += "RETURNING"
         statement += E.idKey
-
+        
         return (
             concatenate(statement),
             bind
+        )
+    }
+
+    public override func drop() -> (String, [Node]) {
+        var statement: [String] = []
+
+        statement += "DROP TABLE IF EXISTS"
+        statement += escape(E.entity)
+        statement += "CASCADE" // added for PostgreSQL
+
+        return (
+            concatenate(statement),
+            []
         )
     }
 
@@ -115,13 +136,77 @@ public final class PostgreSQLSerializer<E: Entity>: GeneralSQLSerializer<E> {
         
         return statement.joined(separator: " ")
     }
-    
+
+    public override func filter(_ filter: Filter) -> (String, [Node]) {
+        var statement: [String] = []
+        var values: [Node] = []
+
+        switch filter.method {
+        case .compare(let key, let c, let value):
+            // `.null` needs special handling in the case of `.equals` or `.notEquals`.
+            if c == .equals && value == .null {
+                statement += escape(filter.entity.entity) + "." + escape(key) + " IS NULL"
+            }
+            else if c == .notEquals && value == .null {
+                statement += escape(filter.entity.entity) + "." + escape(key) + " IS NOT NULL"
+            }
+            else {
+                // Augment pos
+                self.positionalIndex += 1
+
+                statement += escape(filter.entity.entity) + "." + escape(key)
+                statement += comparison(c)
+                // Use the positionalIndex instead of "?"
+                statement += "$\(self.positionalIndex)"
+
+                // `.like` comparison operator requires additional
+                // processing of `value`
+                switch c {
+                case .hasPrefix:
+                    values += hasPrefix(value)
+                case .hasSuffix:
+                    values += hasSuffix(value)
+                case .contains:
+                    values += contains(value)
+                default:
+                    values += value
+                }
+            }
+        case .subset(let key, let s, let subValues):
+            statement += escape(filter.entity.entity) + "." + escape(key)
+            statement += scope(s)
+            statement += placeholders(subValues)
+            values += subValues
+        case .group(let relation, let f):
+            if f.count == 0 {
+                // empty subqueries should result to false to protect
+                // unfiltered data from being returned
+                statement += "false"
+            } else {
+                let (clause, subvals) = filters(f, relation)
+                statement += "(\(clause))"
+                values += subvals
+            }
+        }
+
+        return (
+            concatenate(statement),
+            values
+        )
+    }
+
     public override func placeholder(_ value: Node) -> String {
         return nextPlaceholder
     }
-    
+
     private var nextPlaceholder: String {
         positionalIndex += 1
         return "$\(positionalIndex)"
     }
+
+    // Not needed?
+    // public override func sql(_ string: String) -> String {
+    //     return "\(string)"
+    // }
+
 }
