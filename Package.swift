4f6e323a8e57ec9fa91d33ba4b1ef7a4f5668da1
pruthvikar <pruthvikar@gmail.com>
2017-05-18 00:31:21 +0200

    Update Package.swift (#31)
    Update Package.swift to use Vapor 2 release dependencies

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ let package = Package(
     name: "PostgreSQLDriver",
     dependencies: [
         // PostgreSQL interface for Swift.
-        .Package(url: "https://github.com/vapor-community/postgresql.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        .Package(url: "https://github.com/vapor-community/postgresql.git", majorVersion: 2),
 
-        // Swift models, relationships, and querying for NoSQL and SQL databases.
-        .Package(url: "https://github.com/vapor/fluent.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
+        // Swift models, relationships, and querying for NoSQL and SQL databases
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
 
         // Random number generation
-        .Package(url: "https://github.com/vapor/random.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/random.git", majorVersion: 1)
     ]
 )
