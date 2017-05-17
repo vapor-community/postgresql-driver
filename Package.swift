43c184b497407b4850bbf5202a6872f65b835f9d
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-05-18 00:13:14 +0200

    Trigger CI

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         // PostgreSQL interface for Swift.
         .Package(url: "https://github.com/vapor-community/postgresql.git", majorVersion: 2),
 
-        // Swift models, relationships, and querying for NoSQL and SQL databases.
+        // Swift models, relationships, and querying for NoSQL and SQL databases
         .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
 
         // Random number generation
