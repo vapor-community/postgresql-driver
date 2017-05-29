49a748024ac19918477d685d9ac5f1db19f5b25e
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-05-30 00:05:13 +0200

    Testing improvements and BYTEA issue reproduction (#32)
    * Update insert test for FluentTester 2.0.1+
    * Add failing test that saves an [UInt8] with Fluent [skip ci]
    * Fix init(row:) by explicitly pulling the content as a bytes node
    * Fixed bytes to correctly be inserted

--- Tests/PostgreSQLDriverTests/FluentPostgreSQLTests.swift
+++ Tests/PostgreSQLDriverTests/FluentPostgreSQLTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 @testable import PostgreSQLDriver
 import FluentTester
+import Random
 
 class FluentPostgreSQLTests: XCTestCase {
     func testAll() throws {
@@ -46,16 +47,52 @@ class FluentPostgreSQLTests: XCTestCase {
             try! database.delete(Atom.self)
         }
         
+        let id = UUID()
         try Atom.prepare(database)
-        let atom = Atom(id: Identifier(5), name: "Test", protons: 3, weight: 1.5)
+        let atom = Atom(id: Identifier(id.uuidString), name: "Test", protons: 3, weight: 1.5)
         try atom.save()
         
-        XCTAssertNotNil(try Atom.makeQuery().find(5))
+        XCTAssertNotNil(try Atom.makeQuery().find(id))
+    }
+
+    func testInsertBinaryData() throws {
+        do {
+            let driver = PostgreSQLDriver.Driver.makeTest()
+            let database = Database(driver)
+
+            defer {
+                try! database.delete(Photo.self)
+            }
+
+            try Photo.prepare(database)
+
+            // harder test: try OSRandom.bytes(count: 10_000_000)
+            let randomData: [UInt8] = [48, 49, 50, 51, 52]
+
+            let photo = Photo(title: "The Treachery of Images", content: randomData)
+            try photo.save()
+
+            guard let id = photo.id else {
+                XCTFail("ID could not be retrieved for the new Photo record")
+                return
+            }
+
+            guard let retrieved = try Photo.makeQuery().find(id) else {
+                XCTFail("Failed to retrieve Photo with ID: \(id)")
+                return
+            }
+
+            XCTAssertEqual(randomData, retrieved.content, "Binary content did not match")
+        } catch {
+            // TODO: this is only to see the useful error description while debugging the test
+            XCTFail("\(error)")
+        }
     }
 
     static let allTests = [
         ("testAll", testAll),
         ("testForeignKey", testForeignKey),
-        ("testInsertWithId", testInsertWithId)
+        ("testInsertWithId", testInsertWithId),
+        ("testInsertBinaryData", testInsertBinaryData)
     ]
 }
