fe47c71244bd9071785c53dd19481547e13187a9
Steven Roebert <steven@roebert.nl>
2017-09-18 16:25:17 +0200

    Updated for Swift 4

---
+++ Package@swift-4.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "PostgreSQLDriver",
+    products: [
+        .library(name: "PostgreSQLDriver", targets: ["PostgreSQLDriver"]),
+    ],
+    dependencies: [
+        // PostgreSQL interface for Swift.
+        .package(url: "https://github.com/vapor-community/postgresql.git", .upToNextMajor(from: "2.1.0")),
+        
+        // Swift models, relationships, and querying for NoSQL and SQL databases
+        .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "2.4.0")),
+
+        // Random number generation
+        .package(url: "https://github.com/vapor/random.git", .upToNextMajor(from: "1.2.0"))
+    ],
+    targets: [
+        .target(name: "PostgreSQLDriver", dependencies: ["PostgreSQL", "Fluent", "Random"]),
+        .testTarget(name: "PostgreSQLDriverTests", dependencies: ["PostgreSQLDriver"]),
+    ]
+)
