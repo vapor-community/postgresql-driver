649147cd9e2137bdffcd71610dffda06457e23b1
Zsolt Váradi <vzsg@users.noreply.github.com>
2018-01-16 18:00:45 +0100

    Depend on postgresql from vapor-community directly

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentPostgreSQL",
     dependencies: [
-        .Package(url: "https://github.com/vapor/postgresql.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor-community/postgresql.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1)
     ]
 )
